5cc62f3b4a1bcdd57c657fd87e36937d353cf90a
Pedro Lucas N. Souza <145301330+pedrinsza@users.noreply.github.com>
2024-03-20 16:06:25 -0300

    Add files via upload

---
+++ modelo-cond/app.js
@@ -0,0 +1,13 @@
+// (cond) ? [true] : [false];
+
+let a = 10
+let b = '10'
+let c = 11
+
+console.log((a == b) ? 'A é igual a B' : 'A não é igual a B');
+
+let aux = (a===b) ? 'A é idêntico a B' : 'A não é idêntico a B';
+console.log(aux);
+
+aux = (a>c) ? 'A é maior que C' : 'A é menor que C';
+console.log(aux)
---
+++ modelo-input-form/app.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById('btn')
+let div = document.getElementById('resultado')
+let nomeInput = document.getElementById('nome');
+
+function calcular() {
+
+}
+function limparForm(){
+
+}
+
+btn.addEventListener('click', calcular)
